38c85eacbfe447ea27867c007c8d2ec33d0898e2
AdamPiotrM <123541236+AdamPiotrM@users.noreply.github.com>
2023-10-05 11:48:54 +0200

    V0.2

--- hub/temp.py
+++ hub/temp.py
@@ -1,9 +1,14 @@
 import time
 import machine
 import onewire, ds18x20
+import json
 
-# the device is on GPIO16
-dat = machine.Pin(16) #placeholder, to be changed to read from config file
+with open("config.json", "r") as file:
+    pin = file[Pin]
+    RR = file["interval"] # RR = Refresh Rate
+
+#dat = machine.Pin(16) #placeholder, to be changed to read from config file
+dat = machine.Pin(pin)
 
 # create the onewire object
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
@@ -13,13 +18,23 @@ ds = ds18x20.DS18X20(onewire.OneWire(dat))
 roms = ds.scan()
 print('found devices:', roms)
 
+# gets sensor and device id's and translates them into hex
 sensor_id = hex(int.from_bytes(b'(\xabG8N \x01\x0c', 'little'))
+device_id = hex(int.from_bytes(b'(\xe6ad\x08C% &)', 'little'))
 
 
-# loop 10 times and print all temperatures
-for i in range(10):
+
+#placeholder if json doesn't work
+#while True:
+#    ds.convert_temp()
+#    time.sleep_ms(750)
+#    for rom in roms:
+#        print( device_id, sensor_id, ds.read_temp(rom), end='\n') # Needs some testing
+
+
+#prints while powered
+while True:
     ds.convert_temp()
-    time.sleep_ms(750) #placeholder, to be changed to read from config file
+    time.sleep_ms(RR) 
     for rom in roms:
-        print(sensor_id, ds.read_temp(rom * 1000), end=' ') # Needs some testing
-    print()
+       print( device_id, sensor_id, ds.read_temp(rom), end='\n') 
